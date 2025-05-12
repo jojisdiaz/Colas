bb068a2c8792443ddd4e81200b72491236e4903f
011-Edwar <edwardnasa84@gmail.com>
2025-05-12 08:10:52 -0500

    colas

---
+++ colas.php
@@ -0,0 +1,18 @@
+<?php
+$cola = array();
+
+array_push ($cola,"colombia");
+array_push ($cola,"españa");
+array_push ($cola,"chile");
+array_push ($cola,"peru");
+array_push ($cola,"panama");
+array_push ($cola,"mexico");
+array_push ($cola,"francia");
+array_push ($cola,"portugal");
+array_push ($cola,"brasil");
+
+$paises = array_shift ($cola);
+
+echo"el primer elemento ingresado en la cola es : ".$paises,
+
+?>
